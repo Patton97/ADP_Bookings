e1aab6f7b507042a2491b71e56aa44102e6bea78
Patton97 <atpatton97@gmail.com>
2020-01-09 19:35:18 +0000

    Installed Moq & AutoFac for testing purposes

--- ADP_Bookings_Tests/Presenters/CompanyPresenter_Tests.cs
+++ ADP_Bookings_Tests/Presenters/CompanyPresenter_Tests.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ADP_Bookings.Presenters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac.Extras.Moq;
 
-namespace ADP_Bookings_Tests.Presenters
+using ADP_Bookings.Views; //Enable mocking of views/forms
+using ADP_Bookings.Models;//
+
+namespace ADP_Bookings.Presenters.Tests
 {
-    class CompanyPresenter_Tests
+    [TestClass()]
+    public class CompanyPresenter_Tests
     {
+        [TestMethod()]
+        public void CreateCompany_Test()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ICompanyRepository>()
+                    .Setup(x => x.Add(company);
+                CompanyPresenter presenter = new CompanyPresenter(mock);
+            }
+                
+        }
     }
 }
